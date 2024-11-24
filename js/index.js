ca558a8a412ead2ea401b918265a4f76833dfc82
nilton <nilferraz@gmail.com>
2024-11-24 15:28:42 -0300

    first commit

--- js/index.js
+++ js/index.js
@@ -3,14 +3,14 @@ import { Router } from "./router.js"
 const router = new Router()
 
 router.add("/", "/pages/home.html")
-router.add("/universo", "/pages/universo.html")
-router.add("/exploracao", "/pages/exploracao.html")
+router.add("/universe", "/pages/universe.html")
+router.add("/exploration", "/pages/exploration.html")
 
 router.handle()
 
 window.onpopstate = () => router.handle()
 window.route = () => router.route()
 
-document.querySelector('.elemento').addEventListener('click', function() {
-  this.classList.toggle('marcado');
+document.querySelector('.element').addEventListener('click', function() {
+  this.classList.toggle('mark');
 });
